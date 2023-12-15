8851d25a078ade1a5eaf5ff925cc4a4dbe61c424
Thomas Margotteau <thomas.margotteau@edu.ece.fr>
2023-12-15 18:10:47 +0100

    Aruco qui reconnait carte

--- aruco.py
+++ aruco.py
@@ -17,10 +17,21 @@ while True:
 
     if ids is not None:
         frame = aruco.drawDetectedMarkers(frame, corners, ids)
-        if ids[0]==132:
-            print("Notre Aruco")
+        TextOnScreen="Inconnu"
+        if ids[0] == 132:
+            TextOnScreen = "NOUS"
+        if ids[0] == 0:
+            TextOnScreen = "HGCarte"
+        if ids[0] == 1:
+            TextOnScreen = "HDCarte"
+        if ids[0] == 2:
+            TextOnScreen = "BDCarte"
+        if ids[0] == 3:
+            TextOnScreen = "BGCarte"
+        frame = cv2.putText(img=frame, text=TextOnScreen, org=(int(corners[0][0][0][0]),int(corners[0][0][0][1])), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3.0, color=(255,0,0),thickness=2)
 
-    cv2.imshow('ArUco Detection', frame)
+
+    cv2.imshow('Aruco Detection', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
