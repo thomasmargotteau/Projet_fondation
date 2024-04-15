3caa96e096f1f8c664fd02601ea944420463e340
ArthurMangin <arthur.mangin@edu.ece.fr>
2024-04-15 02:19:31 +0200

    Code clean avec hitbox, différents masques (manque blanc), resizing, cropping et colorisation de l'image

--- pythonProject/Cal_couleurs.py
+++ pythonProject/Cal_couleurs.py
@@ -17,14 +17,8 @@ def calculate_perspective_transform_matrix(Corner1, Corner2, Corner3, Corner4, s
 
 
 def get_mean_hsv(image, center):
-    """
-    Get the mean HSV value in a 5 pixel radius circle around a specified center.
-    Args:
-        image: Input image in BGR format.
-        center: Tuple containing the (x, y) coordinates of the circle center.
-    Returns:
-        Tuple containing the mean HSV value (Hue, Saturation, Value).
-    """
+    #Get the mean HSV value in a 5 pixel radius circle around a specified center.
+
     x, y = center
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     circle_mask = np.zeros_like(hsv_image[:, :, 0], dtype=np.uint8)
@@ -34,14 +28,8 @@ def get_mean_hsv(image, center):
 
 
 def get_mean_bgr(image, center):
-    """
-    Get the mean HSV value in a 5 pixel radius circle around a specified center.
-    Args:
-        image: Input image in BGR format.
-        center: Tuple containing the (x, y) coordinates of the circle center.
-    Returns:
-        Tuple containing the mean HSV value (Hue, Saturation, Value).
-    """
+    #Get the mean BGR value in a 5 pixel radius circle around a specified center.
+
     x, y = center
     bgr_image = image
     circle_mask = np.zeros_like(bgr_image[:, :, 0], dtype=np.uint8)
@@ -50,36 +38,41 @@ def get_mean_bgr(image, center):
     return tuple(map(round, mean_bgr))
 
 
-# Mettre à 0 pour camera, 1 pour photos
+# 0 for camera, 1 for photos
 TEST = 1
 
 final_size = (1200, 800)
 
-# Taille réelle de l'ArUco en millimètres
-taille_reelle_aruco_mm = 42.0  # Modifier cette valeur selon la taille réelle de votre ArUco
 
 if TEST != 1:
     cap = cv2.VideoCapture(0)
 
+# Defintion of the aruco detection parameters
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
 
 parameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, parameters)
 
 nomImages = (
-"Images\PhotoCam2_1.jpg", "Images\PhotoCarteRobot5.jpg", "Images\PhotoCarteRobot9.jpg", "Images\PhotoCarteRobot1.jpg",
-"Images\PhotoCarteRobot2.jpg", "Images\PhotoCarteRobot3.jpg", "Images\PhotoCarteRobot6.jpg",
-"Images\PhotoCarteRobot8.jpg", "Images\PhotoCarteRobot10.jpg", "Images\ConfigBlocsAvecArucoCote1.jpg",
-"Images\PhotoCarteTelephone5.jpg",
-"Images\ConfigBlocsAvecArucoCote3.jpg", "Images\ConfigBlocsAvecArucoDessusLoin1.jpg",
-"Images\ConfigBlocsAvecArucoDessusProche1.jpg")
+    "Images\PhotoCam2_1.jpg",                              #cpt =  0
+    "Images\PhotoCarteRobot5.jpg",                         #cpt =  1
+    "Images\PhotoCarteRobot9.jpg",                         #cpt =  2
+    "Images\PhotoCarteRobot1.jpg",                         #cpt =  3
+    "Images\PhotoCarteRobot2.jpg",                         #cpt =  4
+    "Images\PhotoCarteRobot3.jpg",                         #cpt =  5
+    "Images\PhotoCarteRobot6.jpg",                         #cpt =  6
+    "Images\PhotoCarteRobot8.jpg",                         #cpt =  7
+    "Images\PhotoCarteRobot10.jpg",                        #cpt =  8
+    "Images\ConfigBlocsAvecArucoCote1.jpg",                #cpt =  9
+    "Images\PhotoCarteTelephone5.jpg",                     #cpt =  10
+    "Images\ConfigBlocsAvecArucoCote3.jpg",                #cpt =  11
+    "Images\ConfigBlocsAvecArucoDessusLoin1.jpg",          #cpt =  12
+    "Images\ConfigBlocsAvecArucoDessusProche1.jpg")        #cpt =  13
 nbImages = len(nomImages)
 
 frame = cv2.imread(nomImages[0])
 
-cpt = 2
-
-# 130*120      der = 30   h = b = 55
+cpt = 5
 
 while True:
     if TEST == 0:
@@ -87,7 +80,7 @@ while True:
     elif TEST == 1:
         frame = cv2.imread(nomImages[cpt])
         frame = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
-        if cv2.waitKey(1) & 0xFF == ord('p'):
+        if cv2.waitKey(1) & 0xFF == ord('p'):                   # While the code is running press 'p' to go to the next image
             cpt += 1
             if cpt == nbImages:
                 cpt = 0
@@ -96,65 +89,60 @@ while True:
     corners, ids, rejectedCandidates = detector.detectMarkers(gray)
 
     if ids is not None:
-        # Recherche des indices des marqueurs BGCarte et BDCarte dans la liste ids
-        bg_index = None
-        bd_index = None
         Corner1 = None
         Corner2 = None
         Corner3 = None
         Corner4 = None
         for i in range(len(ids)):
             if ids[i] == 0:
-                p1, p2, p3, p4 = corners[i][0]  # Extract corner points
+                p1, p2, p3, p4 = corners[i][0]  # Extract corner1 points
                 Corner1 = p1
             elif ids[i] == 1:
-                p1, p2, p3, p4 = corners[i][0]  # Extract corner points
+                p1, p2, p3, p4 = corners[i][0]  # Extract corner2 points
                 Corner2 = p2
             elif ids[i] == 2:
-                p1, p2, p3, p4 = corners[i][0]  # Extract corner points
+                p1, p2, p3, p4 = corners[i][0]  # Extract corner3 points
                 Corner3 = p3
             elif ids[i] == 3:
-                p1, p2, p3, p4 = corners[i][0]  # Extract corner points
+                p1, p2, p3, p4 = corners[i][0]  # Extract corner4 points
                 Corner4 = p4
 
         M = calculate_perspective_transform_matrix(Corner1, Corner2, Corner4, Corner3, final_size)
-
+        hit_img = frame.copy()
         for i in range(len(ids)):
-            TextOnScreen = "Inconnu"
-            ColorText = (0, 0, 0)
             if ids[i] == 132:
-                cv2.rectangle(frame, (int(corners[i][0][0][0]), int(corners[i][0][0][1])),
-                              (int(corners[i][0][2][0]), int(corners[i][0][2][1])),
-                              (170, 0, 190), -1)
-            else:
-                TextOnScreen = None
+                # Calculate the midpoint between the bottom two corners
+                bottom_midpoint_x = (int(corners[i][0][2][0]) + int(corners[i][0][3][0])) // 2
+                bottom_midpoint_y = (int(corners[i][0][2][1]) + int(corners[i][0][3][1])) // 2
 
+                # Calculate the direction vector from the bottom two corners
+                direction_vector_x = int(corners[i][0][2][0]) - int(corners[i][0][3][0])
+                direction_vector_y = int(corners[i][0][2][1]) - int(corners[i][0][3][1])
 
+                # Normalize the direction vector
+                direction_vector_length = np.sqrt(direction_vector_x ** 2 + direction_vector_y ** 2)
+                normalized_direction_vector_x = direction_vector_x / direction_vector_length
+                normalized_direction_vector_y = direction_vector_y / direction_vector_length
 
+                # Move the midpoint 10 pixels in front of the equidistance of the bottom two corners
+                new_center_x = bottom_midpoint_x + int(65 * normalized_direction_vector_x)
+                new_center_y = bottom_midpoint_y + int(65 * normalized_direction_vector_y)
+                # Draw a circle in front of the aruco (New Hitbox)
+                cv2.circle(frame, (new_center_x, new_center_y),
+                           115,
+                           (0, 0, 0), -1)
 
-        if ids is not None and 132 in ids:
-            # Find the index of the marker with ID 132
-            idx = list(ids).index(132)
-
-            # Get the corners of the detected marker
-
-            # Draw a black filled rectangle over the detected marker
-            cv2.rectangle(frame, (int(corners[idx][0][0][0]), int(corners[idx][0][0][1])),
-                          (int(corners[idx][0][0][0]) + 130, int(corners[idx][0][0][1]) + 120),
-                          (0, 0, 0), -1)
-
-            # Display the image with the rectangle drawn
-
-        '''else:
-            print("Marker with ID 132 not found.")'''
         dst = cv2.warpPerspective(frame, M, final_size)
+        hit_img = cv2.warpPerspective(hit_img, M, final_size)
 
+        # Saving the images to a file
         cv2.imwrite("Images\Frame.jpg", frame)
         cv2.imwrite("Images\Transformed_image.jpg", dst)
+        cv2.imwrite("Images\Hitbox.jpg", hit_img)
 
         # Open the image
         image = cv2.imread(
-            "Images/Transformed_image.jpg")  # Replace "Images/Transformed_image.jpg" with the path to your image file
+            "Images/Transformed_image.jpg")
 
         # Define the dimensions of the centered rectangle
         crop_width = 1110
@@ -170,8 +158,10 @@ while True:
         cropped_image = image[top:bottom, left:right]
 
         # Save the cropped image
-        cv2.imwrite("Images/cropped_frame.jpg", cropped_image)  # Save the cropped image to a file
+        cv2.imwrite("Images/cropped_frame.jpg", cropped_image)
 
+        #Displaying different results
+        cv2.imshow('Hitbox', hit_img)
         cv2.imshow('Perspective Transformation Result', dst)
         cv2.imshow('Cropped image', cropped_image)
 
@@ -183,6 +173,7 @@ while True:
         circle_radius = 10
         # Get the mean HSV values for each circle
         mean_hsv_values = []
+        # Get the mean BGR values for each circle
         mean_bgr_values = []
 
         for center in circle_centers:
@@ -194,20 +185,23 @@ while True:
             mean_bgr_values.append(mean_bgr)
 
         # Print the mean HSV values
-        '''for i in range(5):
-            print(f"Circle {i + 1}: Mean HSV = {mean_hsv_values[i][0]} {mean_hsv_values[i][1]} {mean_hsv_values[i][2]}")
+        for i in range(5):
+            '''print(f"Circle {i + 1}: Mean HSV = {mean_hsv_values[i][0]} {mean_hsv_values[i][1]} {mean_hsv_values[i][2]}")'''
+            # Convert HSV mean values to BGR in order to draw on the frame 'img_copy'
             bgr_color = cv2.cvtColor(
                 np.uint8([[[mean_hsv_values[i][0], mean_hsv_values[i][1], mean_hsv_values[i][2]]]]), cv2.COLOR_HSV2BGR)
             bgr_color = (int(bgr_color[0][0][0]), int(bgr_color[0][0][1]), int(bgr_color[0][0][2]))
+            # Draw filled circles
             cv2.circle(img_copy, (circle_centers[i][0], circle_centers[i][1]), circle_radius, bgr_color, -1)
 
             cv2.circle(img_copy, (circle_centers_grey[i][0], circle_centers_grey[i][1]), circle_radius,
-                       (mean_bgr_values[i][0], mean_bgr_values[i][1], mean_bgr_values[i][2]), -1)  # Draw filled circles'''
+                       (mean_bgr_values[i][0], mean_bgr_values[i][1], mean_bgr_values[i][2]), -1)  # Draw filled circles
 
-        '''cv2.imshow("Image with Circles", img_copy)'''
+        cv2.imshow("Color calibration", img_copy)
 
         hsv_img = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2HSV)
         for i in range(5):
+        # Definition of mask boundaries with the previously acquired mean values
             if i == 1:
                 # Blue
                 bound_lower = np.array(
@@ -235,7 +229,7 @@ while True:
                 grey_mask = cv2.inRange(cropped_image, bound_lower, bound_upper)
 
         kernel = np.ones((7, 7), np.uint8)
-
+        # Definition and displaying of different masks
         blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_CLOSE, kernel)
         blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
 
@@ -256,10 +250,10 @@ while True:
 
         display_grey_mask = cv2.bitwise_and(cropped_image, cropped_image, mask=grey_mask)
 
-        '''cv2.imshow("Blue mask", display_blue_mask)
+        cv2.imshow("Blue mask", display_blue_mask)
         cv2.imshow("Magenta mask", display_magenta_mask)
         cv2.imshow("Red mask", display_red_mask)
-        cv2.imshow("Grey mask", display_grey_mask)'''
+        cv2.imshow("Grey mask", display_grey_mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
